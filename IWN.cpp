c21e4f9e22d8c8d589e6d5110ecf579e434b1994
AleshaFozu <132448722+Fozu7916@users.noreply.github.com>
2023-07-20 22:33:00 +0700

    Add files via upload

---
+++ IWN.cpp
@@ -0,0 +1,34 @@
+//#include "IWN.h"
+//#include <iostream>
+//
+//int Calculating()
+//{
+//	int a;  char h;  int b;
+//	std::cin >> a >> h >> b;
+//	if (h == '+')
+//	{
+//		
+//		std::cout<< a + b << std::endl;
+//		return a + b;
+//	}
+//	else if (h == '-')
+//	{
+//		
+//		std::cout << a + b << std::endl;
+//		return a - b;
+//	}
+//	else if (h == '*')
+//	{
+//		
+//		std::cout << a + b << std::endl;
+//		return a * b;
+//	}
+//	else if (h == '/')
+//	{
+//		double Da = a;
+//		double Db = b;
+//		
+//		std::cout << Da / Db << std::endl;
+//		return Da / Db;
+//	}	
+//}
